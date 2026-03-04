3bea4ed9b59342b9c21b8c80f6ad2aca43c17ea0
Liam <champiepier@gmail.com>
2026-03-04 09:11:02 -0500

    Working on PLayer Movement 2

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
 
-@export var friction: float = 0.0
+@onready var camera_2d: Camera2D = $"../Camera2D"
+
+@export var friction: float = 0.15
 @export var mass: float = 2.5
+@export var acceleration: float = 700.0
+@export var rotation_speed = 10.0
+
+enum STATES {Normal, Ball, Dart}
+
+var state = STATES.Normal
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var current_frame: int = 0
@@ -9,12 +17,12 @@ var momentum: float
 var speed: int = 200
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(gravity * delta * speed, gravity * delta)
+	velocity = Vector2(gravity * delta * speed, gravity * delta * 5)
 	
 	momentum = velocity.x * mass
 	
-	if Input.is_action_pressed("action"):
-		velocity = Vector2(10, 500)
+	var input_dir := Input.get_axis("up", "down")
+	rotation += input_dir * rotation_speed * delta
 	
 	move_and_slide()
 		

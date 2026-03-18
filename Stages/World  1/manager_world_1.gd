012da2e8f0d6013f0856b618e1f7c1104d9aa1b6
champiepier <champiepier@gmail.com>
2026-03-18 19:21:30 -0400

    Updated Ground Art - Still Placeholder

--- Stages/World  1/manager_world_1.gd
+++ Stages/World  1/manager_world_1.gd
@@ -2,15 +2,8 @@ extends Node
 
 @onready var can_change_again_bar: ProgressBar = $"../CanvasLayer/GUI/MarginContainer/ProgressBarContainer/CanChangeAgainBar"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	Global.call_deferred("get_position_points", %Player)
 	
 	can_change_again_bar.value = Global.state_change_time_left
-
-func _on_timer_timeout() -> void:
-	%Player.can_go = true
